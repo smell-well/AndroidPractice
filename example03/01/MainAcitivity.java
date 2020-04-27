c5fdb83b40842b0b587ec9327c71ccfdbd1cafff
smell-well <58506662+smell-well@users.noreply.github.com>
2020-04-27 20:16:53 +0800

    Create MainAcitivity.java

---
+++ example03/01/MainAcitivity.java
@@ -0,0 +1,44 @@
+package cn.edu.zjut.demo2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        String s = "点击了";
+        Toast.makeText(MainActivity.this,s+item.getTitle(),Toast.LENGTH_SHORT).show();
+
+        /*
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.item01) {
+            Toast.makeText(MainActivity.this,s+item.getTitle(),Toast.LENGTH_SHORT);
+        }
+        */
+
+        return super.onOptionsItemSelected(item);
+    }
+}
